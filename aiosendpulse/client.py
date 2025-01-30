b533678ddc1a33b2404df7049710eb26acfd0aa3
artemprotaev <a.protaev@atomgroup.io>
2025-01-30 14:24:58 +0300

    add token storage

--- aiosendpulse/client.py
+++ aiosendpulse/client.py
@@ -5,11 +5,12 @@ from typing import Literal, Union
 from httpx import AsyncClient
 
 from aiosendpulse.auth import BearerTokenAuth
-from aiosendpulse.methods import (
-    Authorize,
-)
+from aiosendpulse.logger import logger
+from aiosendpulse.methods import Authorize
 from aiosendpulse.services.email import EmailService
 from aiosendpulse.services.smtp import SmtpService
+from aiosendpulse.storage.base import BaseTokenStorage
+from aiosendpulse.storage.exceptions import CacheUnavailableError
 
 
 __all__ = ["AioSendPulseClient"]
@@ -23,11 +24,13 @@ class AioSendPulseClient:
         self,
         client_id: str,
         client_secret: str,
+        token_storage: BaseTokenStorage,
         grant_type: Literal["client_credentials"] = "client_credentials",
         token: str = None,
     ) -> None:
         self.http_client = AsyncClient(base_url=BASE_URL)
         self.auth_class: type[BearerTokenAuth] = BearerTokenAuth
+        self.__token_cache_backend = token_storage
         self.__client_id = client_id
         self.__client_secret = client_secret
         self.__grant_type = grant_type
@@ -45,26 +48,36 @@ class AioSendPulseClient:
     def auth(self) -> Union[BearerTokenAuth, None]:
         return self.__auth
 
-    async def __aenter__(self) -> AioSendPulseClient:
-        if not self.__auth:
-            await self.authorize()
-        return self
-
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self.close()
-
     async def close(self) -> None:
         await self.http_client.aclose()
 
     async def authorize(self) -> None:
-        method = Authorize(
-            client_id=self.__client_id,
-            client_secret=self.__client_secret,
-            grant_type=self.__grant_type,
-        )
-        response = await method(client=self.http_client)
+        try:
+            token = await self.__token_cache_backend.get()
+        except CacheUnavailableError as e:
+            logger.error(f"Token cache unavailable with error: {e}")
+            token = None
+
+        if token is None:
+            logger.debug("SendPulse token not cached")
+            method = Authorize(
+                client_id=self.__client_id,
+                client_secret=self.__client_secret,
+                grant_type=self.__grant_type,
+            )
+            response = await method(client=self.http_client)
+            token = response.access_token.get_secret_value()
+            ex = response.expires_in
+
+            try:
+                await self.__token_cache_backend.set(token=token, ex=ex)
+            except CacheUnavailableError as e:
+                logger.error(f"Token cache unavailable with error: {e}")
+            else:
+                logger.debug("Cache SendPulse token")
+
         self.__auth = self.auth_class(
-            token=response.access_token.get_secret_value(),
+            token=token,
         )
 
     @property
