429d4b4a0bbdd8a2cc6c82f8c15388447680ff24
artemprotaev <a.protaev@atomgroup.io>
2025-02-03 12:32:38 +0300

    add optional status_explain attr

--- aiosendpulse/types/email.py
+++ aiosendpulse/types/email.py
@@ -11,4 +11,5 @@ __all__ = ["EmailDetail"]
 class EmailDetail(SendPulseObject):
     email: EmailStr
     status: Union[int, None] = None
+    status_explain: Union[str, None] = None
     variables: Union[dict[str, str], None] = None
